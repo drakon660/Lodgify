34daf8f994b9ad8d548bb01d121d73eb82efc5d8
drakon <drakon660@gmail.com>
2024-03-26 13:10:45 +0100

    grpc client

---
+++ ApiApplication.Terminal/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// The port number must match the port of the gRPC server.
+
+using Grpc;
+using Grpc.Net.Client;
+using var channel = GrpcChannel.ForAddress("https://localhost:8443");
+
+var client = new ShowtimeService.ShowtimeServiceClient(channel);
+var reservationsResponse = await client.GetAllReservationsAsync(new GetAllReservationsRequest());
+
+foreach (var reservation in reservationsResponse.Reservations)
+{
+    Console.WriteLine(reservation.ReservationId);    
+}
+
+Console.WriteLine("Hello, World!");
